310fa412c73e9b6fafb635e43bcba4690ac01c3c
Ian Mullins <mullins.89@hotmail.com>
2021-07-16 19:57:08 +0100

    Fix upcoming movies page

--- src/api/tmdb-api.js
+++ src/api/tmdb-api.js
@@ -56,10 +56,12 @@ export const getMovieReviews = (id) => {
     });
 };
 
-export const getUpcoming = () => {
-  return fetch(
+export const getUpcoming = async () => {
+  const response = await fetch(
     `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-  )
-    .then((res) => res.json())
-    .then((json) => json.results);
+  );
+  if (!response.ok) {
+    throw new Error(response.json().message);
+  }
+  return response.json();
 };
